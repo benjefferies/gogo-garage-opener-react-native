995bd90d1961cdfbc895925c5f927d398d595912
Ben Jefferies <benjefferies@echosoft.uk>
2020-04-11 13:37:31 +0100

    Use zeroconf to auto discover config on local network

--- Home.js
+++ Home.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { RefreshControl, ScrollView, View, Text } from "react-native";
+import { RefreshControl, ScrollView, View, Text, ActivityIndicator } from "react-native";
 import { Button, Overlay } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { getState, toggle } from "./GarageService";
 import { isLoggedInOrRefresh, login } from "./LoginService";
-import { isSettingsConfigured, addAutocloseTimes, getAutoCloseTimes } from "./StorageService";
+import { isSettingsConfigured, addAutocloseTimes, getAutoCloseTimes, setSettings } from "./StorageService";
 import ActionButton from 'react-native-action-button';
 import { TextInput } from "react-native-gesture-handler";
 import styles from './Style';
+import Zeroconf from 'react-native-zeroconf'
 
 export default class Home extends React.Component {
 
@@ -16,6 +17,7 @@ export default class Home extends React.Component {
     this.state = {
       doorState: 'Loading',
       refreshing: false,
+      loadingConfig: true,
       autoCloseTimes: [],
       addAutoClose: false
     }
@@ -34,8 +36,40 @@ export default class Home extends React.Component {
   async componentDidMount() {
     const { navigation } = this.props;
     if (!await isSettingsConfigured()) {
-      navigation.navigate("Settings")
+      const zeroconf = new Zeroconf();
+      zeroconf.on('start', () => {
+          console.log('[Start]')
+      });
+      zeroconf.on('resolved', service => {
+          console.log('[resolved]')
+          setSettings(service["txt"]["garage_domain"], service["txt"]["as_domain"], service["txt"]["client_id"])
+          this.setState({loadingConfig: false})
+          console.log('[Set state]', JSON.stringify(service["txt"], null, 2))
+          alert('Automatically discovered configuration')
+      });
+      zeroconf.on('update', service => {
+          console.log('[updated]')
+      });
+      zeroconf.on('found', service => {
+          console.log('[found]')
+          alert("found")
+      });
+      zeroconf.on('stop', async service => {
+          console.log('[stopped]')
+          this.setState({loadingConfig: false})
+          if (!await isSettingsConfigured()) {
+              alert('Could not autodiscover configuration')
+              navigation.navigate("Settings")
+          }
+      });
+      zeroconf.on('error', service => {
+          console.log('[stopped]')
+      });
+      zeroconf.scan('gogo-garage-opener', 'tcp','local.')
+      setTimeout(() => zeroconf.stop(), 30000)
       return
+    } else {
+      this.setState({loadingConfig: false})
     }
     if (!await isLoggedInOrRefresh()) {
       await login()
@@ -101,60 +135,61 @@ export default class Home extends React.Component {
     }
     return (
       <View  style={{flex:1}}>
-      <ScrollView style={{flex:1.8}}
-        refreshControl={
-          <RefreshControl
-            refreshing={this.state.refreshing}
-            onRefresh={() => this._onRefresh(navigation)}
-          />
-        }>
-        <Button buttonStyle={{ marginVertical: 1 }}
-          icon={
-            <Icon
-              name="garage"
-              size={40}
-              color="white"
-            />
-          }
-          title={this.state.doorState}
-          onPress={() => this.togglePressed()}
-        />
-        {autoCloseButtons}
-      </ScrollView>
-      <Overlay isVisible={this.state.addAutoClose}>
-        <View>
-          <Text>How long to do want the auto close to be in seconds?</Text>
-          <TextInput
-            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-            onChangeText={(autoCloseTime) => this.setState({newAutoCloseButtonTime: autoCloseTime})}
-            keyboardType="numeric"
-          />
-          <Button style={styles.button}
-            title="ADD"
-            icon={
-              <Icon
-                name="content-save"
-                size={40}
-                color="white"
+          <ScrollView style={{flex:1.8}}
+            refreshControl={
+              <RefreshControl
+                refreshing={this.state.refreshing}
+                onRefresh={() => this._onRefresh(navigation)}
               />
-            }
-            onPress={() => this.createNewButton(this.state.newAutoCloseButtonTime)} />
-            <Button style={styles.button}
-              title="CLOSE"
+            }>
+            {this.state.loadingConfig ? <ActivityIndicator animating={this.state.loadingConfig} size="large" color="#0000ff" /> : null }
+            <Button buttonStyle={{ marginVertical: 1 }}
               icon={
                 <Icon
-                  name="cancel"
+                  name="garage"
                   size={40}
                   color="white"
                 />
               }
-              onPress={() => this.setState({addAutoClose: false})} />
-          </View>
-      </Overlay>
-      <ActionButton
-          buttonColor="rgba(231,76,60,1)"
-          onPress={() => this.setState({addAutoClose: true})}
-        />
+              title={this.state.doorState}
+              onPress={() => this.togglePressed()}
+            />
+            {autoCloseButtons}
+          </ScrollView>
+          <Overlay isVisible={this.state.addAutoClose}>
+            <View>
+              <Text>How long to do want the auto close to be in seconds?</Text>
+              <TextInput
+                style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                onChangeText={(autoCloseTime) => this.setState({newAutoCloseButtonTime: autoCloseTime})}
+                keyboardType="numeric"
+              />
+              <Button style={styles.button}
+                title="ADD"
+                icon={
+                  <Icon
+                    name="content-save"
+                    size={40}
+                    color="white"
+                  />
+                }
+                onPress={() => this.createNewButton(this.state.newAutoCloseButtonTime)} />
+                <Button style={styles.button}
+                  title="CLOSE"
+                  icon={
+                    <Icon
+                      name="cancel"
+                      size={40}
+                      color="white"
+                    />
+                  }
+                  onPress={() => this.setState({addAutoClose: false})} />
+              </View>
+          </Overlay>
+          <ActionButton
+              buttonColor="rgba(231,76,60,1)"
+              onPress={() => this.setState({addAutoClose: true})}
+            />
       </View>
     );
   }
--- android/app/src/main/java/uk/echosoft/garage/opener/MainApplication.java
+++ android/app/src/main/java/uk/echosoft/garage/opener/MainApplication.java
@@ -3,6 +3,7 @@ package uk.echosoft.garage.opener;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.balthazargronon.RCTZeroconf.ZeroconfReactPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.auth0.react.A0Auth0Package;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ZeroconfReactPackage(),
             new ReanimatedPackage(),
             new VectorIconsPackage(),
             new A0Auth0Package(),
